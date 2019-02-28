ae45896e64d1fcd596301d429b1b092797211a04
antnf3 <antnf3@gmail.com>
2019-02-28 22:43:44 +0900

    open api 사용하여 영화정보 가져오기로 수정

--- graphql/db.js
+++ graphql/db.js
@@ -1,54 +1,15 @@
-let movies = [
-	{
-		id: 0,
-		name: 'anderson0',
-		score: 6,
-	},
-	{
-		id: 1,
-		name: 'anderson1',
-		score: 6,
-	},
-	{
-		id: 2,
-		name: 'anderson2',
-		score: 6,
-	},
-	{
-		id: 3,
-		name: 'anderson3',
-		score: 6,
-	},
-	{
-		id: 4,
-		name: 'anderson4',
-		score: 6,
-	},
-];
+import fetch from 'node-fetch';
+const API_URL = 'https://yts.am/api/v2/list_movies.json?';
 
-export const getMovies = () => movies;
-
-export const getById = id => {
-	const filteredMovies = movies.filter(movie => id === movie.id);
-	return filteredMovies[0];
-};
-
-export const deleteMovie = id => {
-	const cleanedMovies = movies.filter(movie => movie.id !== id);
-	if (movies.length > cleanedMovies.length) {
-		movies = cleanedMovies;
-		return true;
-	} else {
-		return false;
+export const getMovies = (limit, rating) => {
+	let REQUEST_URL = API_URL;
+	if (limit > 0) {
+		REQUEST_URL += `limit=${limit}`;
 	}
-};
-
-export const addMovie = (name, score) => {
-	const newMovie = {
-		id: `${movies.length + 1}`,
-		name,
-		score,
-	};
-	movies.push(newMovie);
-	return newMovie;
+	if (rating > 0) {
+		REQUEST_URL += `&minimum_rating=${rating}`;
+	}
+	return fetch(REQUEST_URL)
+		.then(res => res.json())
+		.then(json => json.data.movies);
 };
